3d08d038e9a89fe4a2fe907a0c9bf96b9259b7c2
Ashwin <gv.ashwin@outlook.com>
2013-04-05 23:37:40 -0700

    Adding class def for singly list node

---
+++ SimpleDS/sList/sListNode.rb
@@ -0,0 +1,15 @@
+class SL_Node
+    attr_accessor :value :next
+    def initialize(value == nil)
+        @value = value
+        @next = nil
+    end
+    def to_s
+        if(@next == nil)
+            "#{value}->NIL"
+        else
+            "#{value}->"
+        end
+    end
+end
+
